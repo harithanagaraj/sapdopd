4d2ed7f0a8328a5939144dc6c421dd7b650f41fa
harithanagaraj <36696201+harithanagaraj@users.noreply.github.com>
2018-03-16 15:52:43 +0530

    prgm
    good

---
+++ file.C
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    long long n;
+    int count = 0;
+
+    printf("Enter an integer: ");
+    scanf("%lld", &n);
+
+    while(n != 0)
+    {
+    int(i=0;i++;i=10);
+
+    
+        
+       
+    }
+
+    printf("Number of digits: %d", count);
+}
